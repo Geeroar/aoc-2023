c53cf03cb1d1417cb1fbb80ed485c8194bf708f0
Ruairí O'Brien <1150322+ruarfff@users.noreply.github.com>
2023-12-07 10:54:03 +0000

    Solve day 7 part 2

--- src/roar/q07.rs
+++ src/roar/q07.rs
@@ -1,105 +1,89 @@
+use crate::utils::parser::FileLines;
 use std::cmp::Ordering;
 use std::collections::{BinaryHeap, HashMap};
-use crate::utils::parser::FileLines;
 
-fn _rank_card(card: char) -> u32 {
+fn _rank_card(card: char, jokers_wild: bool) -> u32 {
     // What am I doing :D
     let card_ranks = HashMap::from([
         ('A', 13),
         ('K', 12),
         ('Q', 11),
-        ('J', 10),
-        ('T', 9),
-        ('9', 8),
-        ('8', 7),
-        ('7', 6),
-        ('6', 5),
-        ('5', 4),
-        ('4', 3),
-        ('3', 2),
-        ('2', 1),
+        ('J', if jokers_wild { 1 } else { 10 }),
+        ('T', if jokers_wild { 10 } else { 9 }),
+        ('9', if jokers_wild { 9 } else { 8 }),
+        ('8', if jokers_wild { 8 } else { 7 }),
+        ('7', if jokers_wild { 7 } else { 6 }),
+        ('6', if jokers_wild { 6 } else { 5 }),
+        ('5', if jokers_wild { 5 } else { 4 }),
+        ('4', if jokers_wild { 4 } else { 3 }),
+        ('3', if jokers_wild { 3 } else { 2 }),
+        ('2', if jokers_wild { 2 } else { 1 }),
     ]);
 
     return card_ranks.get(&card).unwrap().clone();
 }
 
-fn _compare_hands_by_cards(hand1: &String, hand2: &String) -> Ordering {
+fn _compare_hands_by_cards(hand1: &String, hand2: &String, jokers_wild: bool) -> Ordering {
     let hand1_cards: Vec<char> = hand1.chars().collect();
     let hand2_cards: Vec<char> = hand2.chars().collect();
     for (i, card) in hand1_cards.iter().enumerate() {
-        if _rank_card(*card) > _rank_card(hand2_cards[i]) {
+        if _rank_card(*card, jokers_wild) > _rank_card(hand2_cards[i], jokers_wild) {
             return Ordering::Greater;
-        } else if _rank_card(*card) < _rank_card(hand2_cards[i]) {
+        } else if _rank_card(*card, jokers_wild) < _rank_card(hand2_cards[i], jokers_wild) {
             return Ordering::Less;
         }
     }
     return Ordering::Equal;
 }
 
-fn _rank_hand(hand: &String) -> u32 {
-    let hand_ranks = HashMap::from([
-        ("FiveOfAKind", 7),
-        ("FourOfAKind", 6),
-        ("FullHouse", 5),
-        ("ThreeOfAKind", 4),
-        ("TwoPair", 3),
-        ("OnePair", 2),
-        ("HighCard", 1),
-    ]);
-    // Keep track of counts
-    let mut counts = Vec::new();
-    let mut current_count = 0;
-    // Sorting the string to make it easier to check
-    let mut chars: Vec<char> = hand.chars().collect();
-    chars.sort();
-    let mut current_char =  chars[0];
-    for( i, c) in chars.iter().enumerate() {
-        if *c == current_char {
-            current_count += 1;
-        } else {
-            counts.push(current_count);
-            current_char = *c;
-            current_count = 1;
-        }
-        if i == chars.len() - 1 {
-            counts.push(current_count);
-        }
-    }
-    counts.sort_by(|a, b| b.cmp(a));
+fn _rank_hand(hand: &String, jokers_wild: bool) -> u32 {
+    const FIVE_OF_A_KIND: u32 = 7;
+    const FOUR_OF_A_KIND: u32 = 6;
+    const FULL_HOUSE: u32 = 5;
+    const THREE_OF_A_KIND: u32 = 4;
+    const TWO_PAIR: u32 = 3;
+    const ONE_PAIR: u32 = 2;
+    const HIGH_CARD: u32 = 1;
 
-    // Check for FiveOfAKind
-    if counts[0] == 5 {
-        return hand_ranks.get("FiveOfAKind").unwrap().clone();
+    let mut counts = HashMap::new();
+    let mut joker_count = 0;
+
+    for c in hand.chars() {
+        if c == 'J' && jokers_wild {
+            joker_count += 1;
+        } else {
+            *counts.entry(c).or_insert(0) += 1;
+        }
     }
-    // Check for FourOfAKind
-    if counts[0] == 4 {
-        return hand_ranks.get("FourOfAKind").unwrap().clone();
+
+    let mut values: Vec<_> = counts.values().cloned().collect();
+    values.sort_unstable_by(|a, b| b.cmp(a));
+
+    match joker_count {
+        5 => FIVE_OF_A_KIND, // Five of a kind with all jokers
+        _ => {
+            // Use jokers to form the best hand
+            let max_count = match values.get(0) {
+                Some(&x) => x + joker_count,
+                None => joker_count,
+            };
+
+            match (max_count, values.len()) {
+                (5, _) => FIVE_OF_A_KIND,
+                (4, _) => FOUR_OF_A_KIND,
+                (3, 2) | (3, 1) => FULL_HOUSE,
+                (3, _) => THREE_OF_A_KIND,
+                (2, 3) | (2, 2) => TWO_PAIR,
+                (2, _) => ONE_PAIR,
+                _ => HIGH_CARD,
+            }
+        }
     }
-    // Check for FullHouse
-    if counts[0] == 3 && counts[1] == 2 {
-        return hand_ranks.get("FullHouse").unwrap().clone();
-    }
-    // Check for ThreeOfAKind
-    if counts[0] == 3 {
-        return hand_ranks.get("ThreeOfAKind").unwrap().clone();
-    }
-    // Check for TwoPair
-    if counts[0] == 2 && counts[1] == 2 {
-        return hand_ranks.get("TwoPair").unwrap().clone();
-    }
-    // Check for OnePair
-    if counts[0] == 2 {
-        return hand_ranks.get("OnePair").unwrap().clone();
-    }
-    // HighCard
-    return hand_ranks.get("HighCard").unwrap().clone();
 }
 
-
-#[derive(Eq)]
-#[derive(Debug)]
-#[derive(Clone)]
+#[derive(Eq, Debug, Clone)]
 struct HandAndBid {
+    jokers_wild: bool,
     data: (String, u32),
 }
 
@@ -111,8 +95,12 @@ impl PartialEq for HandAndBid {
 
 impl Ord for HandAndBid {
     fn cmp(&self, other: &Self) -> Ordering {
-        match _rank_hand(&self.data.0).cmp(&_rank_hand(&other.data.0)) {
-            Ordering::Equal => _compare_hands_by_cards(&self.data.0, &other.data.0),
+        match _rank_hand(&self.data.0, self.jokers_wild)
+            .cmp(&_rank_hand(&other.data.0, self.jokers_wild))
+        {
+            Ordering::Equal => {
+                _compare_hands_by_cards(&self.data.0, &other.data.0, self.jokers_wild)
+            }
             other_ordering => other_ordering,
         }
     }
@@ -126,7 +114,6 @@ impl PartialOrd for HandAndBid {
 
 struct Input {
     _hands_and_bids: Vec<HandAndBid>,
-    _ordered_hands_and_bids: BinaryHeap<HandAndBid>,
 }
 
 impl TryFrom<FileLines> for Input {
@@ -139,6 +126,7 @@ impl TryFrom<FileLines> for Input {
             let hand = hand_and_bid.0;
             let bid = hand_and_bid.1.parse::<u32>().unwrap();
             let hand_and_bid = HandAndBid {
+                jokers_wild: false,
                 data: (hand.to_string(), bid),
             };
             hands_and_bids.push(hand_and_bid);
@@ -148,7 +136,9 @@ impl TryFrom<FileLines> for Input {
         for hand_and_bid in hands_and_bids.iter() {
             ordered_hands_and_bids.push(hand_and_bid.clone());
         }
-        Ok(Input { _hands_and_bids: hands_and_bids, _ordered_hands_and_bids: ordered_hands_and_bids })
+        Ok(Input {
+            _hands_and_bids: hands_and_bids,
+        })
     }
 }
 
@@ -157,7 +147,6 @@ fn _get_total_winnings(ordered_hands_and_bids: BinaryHeap<HandAndBid>) -> u32 {
     let mut rank = ordered_hands_and_bids.len();
     let mut total = 0;
     while let Some(hand_and_bid) = ordered_hands_and_bids.pop() {
-        println!("{:?}", hand_and_bid);
         total += hand_and_bid.data.1 * rank as u32;
         rank -= 1;
     }
@@ -166,12 +155,24 @@ fn _get_total_winnings(ordered_hands_and_bids: BinaryHeap<HandAndBid>) -> u32 {
 
 fn _part_1(input_file: &str) -> std::io::Result<u32> {
     let input = Input::try_from(FileLines::new(input_file)?)?;
-    Ok(_get_total_winnings(input._ordered_hands_and_bids))
+    let mut ordered_hands_and_bids = BinaryHeap::new();
+    for hand_and_bid in input._hands_and_bids.iter() {
+        ordered_hands_and_bids.push(hand_and_bid.clone());
+    }
+    Ok(_get_total_winnings(ordered_hands_and_bids))
 }
 
 fn _part_2(input_file: &str) -> std::io::Result<u32> {
     let input = Input::try_from(FileLines::new(input_file)?)?;
-    Ok(_get_total_winnings(input._ordered_hands_and_bids))
+    let mut ordered_hands_and_bids = BinaryHeap::new();
+    for hand_and_bid in input._hands_and_bids.iter() {
+        let hand_and_bid_with_jokers_wild = HandAndBid {
+            jokers_wild: true,
+            data: hand_and_bid.data.clone(),
+        };
+        ordered_hands_and_bids.push(hand_and_bid_with_jokers_wild);
+    }
+    Ok(_get_total_winnings(ordered_hands_and_bids))
 }
 
 #[cfg(test)]
@@ -190,18 +191,18 @@ mod tests {
     #[test]
     fn roar_q07_p1_main() {
         let result = _part_1(INPUT);
-        assert_eq!(result.unwrap(), 0);
+        assert_eq!(result.unwrap(), 248453531);
     }
 
     #[test]
     fn roar_q07_p2_sample() {
         let result = _part_2(INPUT_SAMPLE);
-        assert_eq!(result.unwrap(), 0);
+        assert_eq!(result.unwrap(), 5905);
     }
 
     #[test]
     fn roar_q07_p2_main() {
         let result = _part_2(INPUT);
-        assert_eq!(result.unwrap(), 0);
+        assert_eq!(result.unwrap(), 248781813);
     }
 }
