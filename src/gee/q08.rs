a6368a549dff40755a7cf59e16e96bd7c9845044
George Boyle <george@spotify.com>
2023-12-15 16:49:29 -0500

    [gee] Make day 8 part 2 less disgraceful
    Use LCM to get the result instantly instead of the horrendous brute force approach.

--- src/gee/q08.rs
+++ src/gee/q08.rs
@@ -1,6 +1,7 @@
 #![allow(dead_code)]
 
 use crate::utils::parser::FileLines;
+use std::cmp;
 use std::collections::HashMap;
 
 #[derive(Debug)]
@@ -47,6 +48,65 @@ impl TryFrom<FileLines> for Input {
     }
 }
 
+fn find_cycles<'a>(
+    instructions: &[Direction],
+    nodes: &'a HashMap<String, (String, String)>,
+) -> HashMap<&'a str, usize> {
+    let mut cycles: HashMap<&str, usize> = HashMap::new();
+    let mut steps = 0;
+    let mut i = 0;
+    let mut current_nodes: Vec<(&str, &str, Vec<_>)> = nodes
+        .keys()
+        .filter(|s| s.ends_with('A'))
+        .map(|s| (s.as_str(), s.as_str(), vec![("", 0)]))
+        .collect();
+    current_nodes.sort();
+    loop {
+        let instruction = &instructions[i];
+        current_nodes = current_nodes
+            .into_iter()
+            .map(|(s, n, mut v)| match instruction {
+                Direction::Left => {
+                    let x = nodes.get(n).map(|(l, _)| l).unwrap().as_str();
+                    (s, x, v)
+                }
+                Direction::Right => {
+                    let x = nodes.get(n).map(|(_, r)| r).unwrap().as_str();
+                    if x.ends_with('Z') {
+                        v.push((x, steps + 1));
+                        if !cycles.contains_key(s) {
+                            cycles.insert(s, steps + 1);
+                        }
+                    }
+                    (s, x, v)
+                }
+            })
+            .collect();
+        steps += 1;
+        if cycles.len() == current_nodes.len() {
+            return cycles;
+        } else {
+            i = match i {
+                n if n >= instructions.len() - 1 => 0,
+                n => n + 1,
+            };
+        }
+    }
+}
+
+fn lcm(a: usize, b: usize) -> usize {
+    let (small, big) = (cmp::min(a, b), cmp::max(a, b));
+    if big % small == 0 {
+        return big;
+    }
+    for i in 1..small {
+        if big * i % small == 0 {
+            return big * i;
+        }
+    }
+    small * big
+}
+
 fn part_1(input_file: &str) -> std::io::Result<u32> {
     let input = Input::try_from(FileLines::new(input_file)?)?;
     let mut steps = 0;
@@ -71,59 +131,15 @@ fn part_1(input_file: &str) -> std::io::Result<u32> {
     Ok(steps)
 }
 
-fn part_2(input_file: &str) -> std::io::Result<u64> {
+pub fn part_2(input_file: &str) -> std::io::Result<usize> {
     let input = Input::try_from(FileLines::new(input_file)?)?;
-    let mut steps = 0;
-    let mut i = 0;
-    let (instructions, nodes) = (input.instructions, input.nodes);
-    let mut current_nodes: Vec<(&str, &str, Vec<_>)> = nodes
-        .keys()
-        .filter(|s| s.ends_with('A'))
-        .map(|s| (s.as_str(), s.as_str(), vec![("", 0)]))
-        .collect();
-    current_nodes.sort();
-    let mut cycles: HashMap<&str, u64> = HashMap::new();
-    loop {
-        let instruction = &instructions[i];
-        current_nodes = current_nodes
-            .into_iter()
-            .map(|(s, n, mut v)| match instruction {
-                Direction::Left => {
-                    let x = nodes.get(n).map(|(l, _)| l).unwrap().as_str();
-                    (s, x, v)
-                }
-                Direction::Right => {
-                    let x = nodes.get(n).map(|(_, r)| r).unwrap().as_str();
-                    if x.ends_with('Z') {
-                        v.push((x, steps + 1));
-                        if !cycles.contains_key(s) {
-                            cycles.insert(s, steps + 1);
-                        }
-                    }
-                    (s, x, v)
-                }
-            })
-            .collect();
-        steps += 1;
-        if cycles.len() == current_nodes.len() {
-            break;
-        } else {
-            i = match i {
-                n if n >= instructions.len() - 1 => 0,
-                n => n + 1,
-            };
-        }
+    let cycles = find_cycles(&input.instructions, &input.nodes);
+    let cycle_sizes: Vec<usize> = cycles.values().copied().collect();
+    let mut result = 1;
+    for n in cycle_sizes {
+        result = lcm(result, n);
     }
-    steps = 0;
-    let cycle_sizes: Vec<u64> = cycles.values().copied().collect();
-    let largest_jump = cycle_sizes.iter().max().unwrap();
-    loop {
-        steps += largest_jump;
-        if cycle_sizes.iter().all(|&x| steps % x == 0) {
-            break;
-        }
-    }
-    Ok(steps)
+    Ok(result)
 }
 
 #[cfg(test)]
@@ -159,7 +175,6 @@ mod tests {
         assert_eq!(result.unwrap(), 6);
     }
 
-    #[ignore]
     #[test]
     fn gee_q08_p2_main() {
         let result = part_2(INPUT);
